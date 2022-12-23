cc527c7273ea59b9c3920e7cb9d73228e223efce
fedyarays <91377626+fedyarays@users.noreply.github.com>
2022-12-23 12:44:03 +0300

    Add files via upload

--- matrix.py
+++ matrix.py
@@ -1,18 +1,34 @@
 import copy
 import numpy
 
+
 class Matrix:
+    """
+    Класс Matrix:
+    1) Операторы: +(add), -(sub), *(mul), [n:m:](getitem) **-1(pow), ~(invert)
+    2) Методы: транспонирование(trans), удаление строк(delete), перестановка строк(permutation),
+     получение ранга(rang), получение размера(size), вывод матрицы(to_list)
+     Ни одна функция не изменяет исходную матрицу, а возвращает новый объект.
+    """
+
     def __init__(self, matrix: list):
-        "Принимает массив как аргумент"
+        """
+        Создание матрицы. Принимает массив как аргумент, создаёт объект класса Matrix,
+        проверяет, является ли аргумент матрицей
+         """
         for i in range(len(matrix) - 1):
-            if len(matrix[i]) != len(matrix[i + 1]): # Двумерный массив не является матрицей, если длины его элементов разные
+            if len(matrix[i]) != len(matrix[i + 1]):
                 raise ValueError("Несуществующая матрица")
         self.matrix = matrix
 
-    def __add__(self, other):
-        "Осуществляет сложение матриц. Возвращает матрицу"
+    def __add__(self, other):  # СЛОЖЕНИЕ
+        """
+        Осуществляет сложение двух матриц.
+        :param other: вторая матрица.
+        :return: новая матрица.
+        """
         try:
-            c = [] # Создаём пустой массив
+            c = []  # Создаём пустой массив
             for i in range(len(self.to_list())):
                 c.append(list(map(lambda x, y: x + y,
                                   self.to_list()[i],
@@ -21,10 +37,14 @@ class Matrix:
         except IndexError:  # Проверка на Index out of range
             raise IndexError("Матрицы разных размеров")
 
-    def __sub__(self, other):
-        "Осуществляет вычетание матриц. Возвращает матрицу"
+    def __sub__(self, other):  # ВЫЧИТАНИЕ
+        """
+        Осуществляет вычетание двух матриц.
+        :param other: вторая матрица.
+        :return: новая матрица.
+        """
         try:
-            c = [] # Создаём пустой массив
+            c = []  # Создаём пустой массив
             for i in range(len(self.to_list())):
                 c.append(list(map(lambda x, y: x - y,
                                   self.to_list()[i],
@@ -33,88 +53,135 @@ class Matrix:
         except IndexError:  # Проверка на Index out of range
             raise IndexError("Матрицы разных размеров")
 
-    def __mul__(self, other):
-        "Осуществляет умножение матриц на число или матрицу. Возвращает матрицу"
+    def __mul__(self, other):  # УМНОЖЕНИЕ
+        """
+        Осуществляет умножение матриц на число или матрицу.
+        :param other: вторая матрица или число.
+        :return: новая матрица.
+        """
         if isinstance(other, int) or isinstance(other, float):  # Умножение на число
-            matrix = copy.deepcopy(self.to_list()) # Создаём копию исходного массива
+            matrix = copy.deepcopy(self.to_list())  # Создаём копию исходного массива
             for i in range(len(matrix)):
-                matrix[i] = [x * other for x in matrix[i]] # Записываем в копию массива произведение элемента и числа
+                matrix[i] = [x * other for x in matrix[i]]  # Записываем в копию массива произведение элемента и числа
             return Matrix(matrix)
 
-        try: # Умножение на матрицу
+        try:  # Умножение на матрицу
             size_matrix = len(self.to_list())
-            c = [[] for _ in range(size_matrix)] # Создаём пустой двумерный массив
+            c = [[] for _ in range(size_matrix)]  # Создаём пустой двумерный массив
             for i in range(size_matrix ** 2):
                 t_matrix = other.trans().matrix  # Транспонируем матрицу
-                c[i // size_matrix].append(sum(list(map( # Умножаем каждый элемент 1 матрицы на элемент 2 матрицы с тем же индексом
-                                 lambda x, y: x * y,
-                                 self.to_list()[i // size_matrix],
-                                 t_matrix[i % size_matrix]))))
+                c[i // size_matrix].append(
+                    sum(list(map(  # Умножаем каждый элемент 1 матрицы на элемент 2 матрицы с тем же индексом
+                        lambda x, y: x * y,
+                        self.to_list()[i // size_matrix],
+                        t_matrix[i % size_matrix]))))
             return Matrix(c)
         except IndexError:
             raise IndexError("Матрицы разных размеров")
 
-    def __getitem__(self, val):
-        "Осуществляет вывод элементов матрицы. Выводит строки матрицы"
+    def __getitem__(self, val):  # ВЫВОД СТРОК
+        """
+        Осуществляет вывод элементов матрицы. Счет начинается с нуля. mat1[0:1:] - первая строка.
+        Чтобы вывести столбцы, нужно транспонировать матрицу (mat.trans)
+        mat1.trans()[0:1:] - первый столбец. Грубо говоря, выводятся с нулевой по первую строки.
+        :param val: номер строки.
+        :return: список.
+        """
         if isinstance(val, slice):
             return self.matrix[val]
         else:
             raise TypeError("Индексы должны иметь вид [x:y:]")
 
-    def __pow__(self, power=-1):
-        "Осуществляет нахождение обратной матрицы. Возвращает матрицу"
-        if self.__invert__() == 0:
+    def __pow__(self, power=-1):  # ОБРАТНАЯ МАТРИЦА
+        """
+        Осуществляет нахождение обратной матрицы с помощью numpy.
+        :param power: степень, в которую возводим матрицу (по умолчанию -1).
+        :return: новая матрица.
+        """
+        if numpy.linalg.det(self.to_list()) == 0:
             raise ZeroDivisionError("Обратной матрицы не существует")
         try:
+            err = 0
             m = self.to_list()
-            np_inverted = numpy.linalg.inv(m) # Нахождение обратной матрицы с помощью библиотеки numpy
-            inverted = numpy.stack(np_inverted).reshape(len(m), len(m))
+            np_inverted = numpy.linalg.inv(m)  # Нахождение обратной матрицы с помощью библиотеки numpy
+            inv = numpy.stack(np_inverted).tolist()
+            inverted = []
+            k = 0
+            for i in inv:
+                inverted.append([])
+                for j in i:
+                    inverted[k].append(round(j, 5))
+                k += 1
             return Matrix(inverted)
         except numpy.linalg.LinAlgError:
             raise ValueError("Матрица должна быть квадратной")
 
-    def get_copy(self):
-        "Осуществляет создание копии матрицы. Возвращает матрицу"
+    def get_copy(self):  # КОПИЯ
+        """
+        Осуществляет создание копии матрицы.
+        :return: новая матрица.
+        """
         c = copy.deepcopy(self)
         return c
 
-    def __invert__(self):
-        "Осуществляет определителя (детерминанта) матрицы. Возвращает число"
-        if len(self.to_list()) == len(self.to_list()[0]): # Проверка матрицы на квадратность
+    def __invert__(self):  # ДЕТЕРМИНАНТ
+        """
+        Осуществляет нахождение определителя (детерминанта) матрицы.
+        :return: число.
+        """
+        if len(self.to_list()) == len(self.to_list()[0]):  # Проверка матрицы на квадратность
             matrix = copy.deepcopy(self.to_list())
             count = 0
             if len(matrix) == 1:
                 return matrix[0][0]
             else:
                 for i in range(len(matrix)):
-                    m = Matrix(matrix).delete(1, i + 1)
-                    count += matrix[0][i] * m.__invert__() * (-1) ** (i + 2) # Детерминант складывается из произведений по теореме Лапласа
+                    m = Matrix(matrix).delete("строка", 1)
+                    m = m.delete("столбец", i+1)
+                    count += matrix[0][i] * m.__invert__() * (-1) ** (
+                                i + 2)  # Детерминант складывается из произведений по теореме Лапласа
             return count
         else:
             raise ValueError("Матрица должна быть квадратной")
 
-    def trans(self):
-        "Осуществляет транспонирование матрицы. Возвращает матрицу"
-        return Matrix(list(map(list, zip(*self.to_list())))) # Сменить строки на столбцы
+    def trans(self):  # ТРАНСПОНИРОВАНИЕ
+        """
+        Осуществляет транспонирование матрицы.
+        :return: новая матрица.
+        """
+        return Matrix(list(map(list, zip(*self.to_list()))))  # Сменить строки на столбцы
 
-    def delete(self, line, column):
-        "Осуществляет удаление строки или столбца матрицы. Возвращает матрицу"
-        line -= 1
-        column -= 1
+    def delete(self, type, number):  # УДАЛЕНИЕ
+        """
+        Осуществляет удаление строки или столбца матрицы.
+        :param type: "строка" или "столбец"
+        :param number: номер строки или столбца
+        :return: новая матрица.
+        """
+        number -= 1
         matrix = copy.deepcopy(self.to_list())
-        del matrix[line] # Удаление строки
-        for item in range(len(matrix)): # Удаление столбца
-            del matrix[item][column]
+        if type == "строка":
+            del matrix[number]
+        if type == "столбец":
+            matrix = Matrix(matrix).trans().to_list()
+            del matrix[number]
+            matrix = Matrix(matrix).trans().to_list()
         return Matrix(matrix)
 
-    def permutation(self, line_column: str, number_i, number_j):
-        "Осуществляет перестановку строк или столбцов матрицы. Возвращает матрицу"
+    def permutation(self, line_column: str, number_i, number_j):  # ПЕРЕСТАНОВКА
+        """
+        Осуществляет перестановку строк или столбцов матрицы.
+        :param line_column: "строка" или "столбец"
+        :param number_i: номер строки/столбца
+        :param number_j: номер строки/столбца
+        :return: новая матрица.
+        """
         number_i -= 1
         number_j -= 1
-        if line_column.lower() == "строка": # Поменять местами две строки
+        if line_column.lower() == "строка":  # Поменять местами две строки
             matrix = copy.deepcopy(self.to_list())
             matrix[number_i], matrix[number_j] = matrix[number_j], matrix[number_i]
-        elif line_column.lower() == "столбец": # Транспонировать и поменять местами две строки
+        elif line_column.lower() == "столбец":  # Транспонировать и поменять местами две строки
             matrix = Matrix(self.to_list()).trans().matrix
             matrix[number_i], matrix[number_j] = matrix[number_j], matrix[number_i]
             matrix = Matrix(matrix).trans().matrix
@@ -122,63 +189,103 @@ class Matrix:
             raise ValueError("Введите 'строка' или 'столбец'")
         return Matrix(matrix)
 
-    def rang(self):
-        "Осуществляет нахождение ранга матрицы. Возвращает число"
+    def rang(self):  # РАНГ
+        """
+        Осуществляет нахождение ранга матрицы с помощью numpy.
+        :return: число.
+        """
         mat = self.to_list()
-        return numpy.linalg.matrix_rank(mat) # Нахождение ранга с помощью библиотеки numpy
+        return numpy.linalg.matrix_rank(mat)  # Нахождение ранга с помощью библиотеки numpy
 
-    def size(self): # Нахождение размера матрицы
-        "Осуществляет нахождение размера матрицы. Возвращает число"
+    def size(self):  # РАЗМЕР
+        """
+        Осуществляет нахождение размера матрицы.
+        :return: количество строк и количество столбцов.
+        """
         return f"{len(self.to_list())} строк, {len(self.to_list()[0])} столбцов"
 
-    def to_list(self):
-        "Осуществляет вывод матрицы. Возвращает массив"
+    def to_list(self):  # ВЫВОД МАТРИЦЫ
+        """
+        Осуществляет вывод матрицы.
+        :return: лист(массив).
+        """
         return self.matrix
+
 
 # ПРИМЕР РАБОТЫ
 
-array1 = [[0, 1, 3, 4, 5, 6, 6],
-          [2, 1, 7, 4, 5, 9, 6],
-          [0, 1, 3, 10, 5, 6, 6],
-          [15, 1, 3, 11, 5, 9, 5]]
+if __name__ == "__main__":
+    array1 = [[0, 1, 3, 4, 5, 6, 6],
+              [2, 1, 7, 4, 5, 9, 6],
+              [0, 1, 3, 10, 5, 6, 6],
+              [15, 1, 3, 11, 5, 9, 5]]
 
-array2 = [[3, -5],
-          [1, -2],]
+    array2 = [[3, -5],
+              [1, -2]]
 
-array3 = [[3, 3, 3, 3, 5, 6, 6],
-          [2, 1, 7, 66, 5, 9, 6],
-          [0, 1, 3, 70, 5, 6, 6],
-          [15, 1, 23, 431, 5, 9, 5]]
+    array3 = [[3, 3, 3, 3, 5, 6, 6],
+              [2, 1, 7, 66, 5, 9, 6],
+              [0, 1, 3, 70, 5, 6, 6],
+              [15, 1, 23, 431, 5, 9, 5]]
 
-array4 = [[3, 3, 3, 3, 5, 6, 6],
-          [0, 0, 7, 0, 5, 9, 6],
-          [121, 1, 3, 70, 43, 897, 57676],
-          [15, 1, 23, 431, 5, 9, 5]]
+    array4 = [[3, 3, 3, 3, 5, 6, 6],
+              [0, 0, 7, 0, 5, 9, 6],
+              [121, 1, 3, 70, 43, 897, 57676],
+              [15, 1, 23, 431, 5, 9, 5]]
 
-mat1 = Matrix(array1)  # Объект класса Matrix (матрица 1)
-mat2 = Matrix(array2)  # Объект класса Matrix (матрица 2)
-mat3 = Matrix(array3)  # Объект класса Matrix (матрица 3)
-mat4 = Matrix(array4)  # Объект класса Matrix (матрица 4)
+    mat1 = Matrix(array1)
+    mat2 = Matrix(array2)
+    mat3 = Matrix(array3)
+    mat4 = Matrix(array4)
 
-mat_sum = mat1 + mat4
-mat_sum = mat1.__add__(mat4)
+    mat_sum = mat1 + mat4
+    mat_sum = mat1.__add__(mat4)
 
-mat_sub = mat1 - mat4
-mat_sub = mat1.__sub__(mat4)
+    mat_sub = mat1 - mat4
+    mat_sub = mat1.__sub__(mat4)
 
-mat_mul = mat1 * mat3
-mat_mul = mat1.__mul__(mat3)
+    mat_mul = mat1 * mat3
+    mat_mul = mat1.__mul__(mat3)
+    mat_mul1 = mat2 * 10
 
-mat_elem = mat1[1:2:]
+    mat_elem = mat1[1:2:]
 
-mat_inv = mat2 ** -1
-mat_inv = mat2.__pow__(-1)
-"""
-print(mat1.to_list())
-print(mat_sum.to_list())
-print(mat1.rang())
-print((mat2**-1).to_list())
-print(mat1[1:2:])
-print(Matrix.__init__.__doc__)
-print(Matrix.to_list.__doc__)
-"""
+    mat_inv = mat2 ** -1
+    mat_inv = mat2.__pow__(-1)
+
+    mat_determinant = ~mat2
+
+    mat_del = mat1.delete("строка", 1)
+
+    mat_del_2 = mat1.delete("столбец", 1)
+
+    """
+    print("Матрица 1 + Матрица 4: ", mat_sum.to_list())
+    print("Матрица 1 - Матрица 4: ", mat_sub.to_list())
+    print("Матрица 1 * Матрица 4: ", mat_mul.to_list())
+    print("Матрица 2 * 10: ", mat_mul1.to_list())
+    print(mat1[0:1:])
+    print("Обратная матрица 2:", (mat2**-1).to_list())
+    print("Обратная матрица 2:", (mat2.__pow__().to_list()))
+    print("Копия матрицы 2:", mat2.get_copy().to_list())
+    print("Детерминант матрицы 2: ", ~mat2)
+    print("Транспонированная матрица 3: ", mat3.trans().to_list())
+    print("Удаление 1 строки матрицы 1: ", mat_del.to_list())
+    print("Удаление 1 столбца матрицы 1: ", mat_del_2.to_list())
+    print("Перестановка строк 1 и 2 матрицы 4: ", mat4.permutation("строка",1,2).to_list())
+    print("Ранг матрицы 1:", mat1.rang())
+    print("Размер матрицы 1:", mat1.size())
+    print("Матрица 1: ", mat1.to_list())
+    print(Matrix.__init__.__doc__)
+    print(Matrix.to_list.__doc__)
+    """
+
+    print("Работа matrix.py завершена")
+
+
+
+
+
+
+
+
--- matrix_tests.py
+++ matrix_tests.py
@@ -1,4 +1,6 @@
 from matrix import Matrix
+from pytest import *
+import numpy
 
 array1 = [[0, 1, 3, 4, 5, 6, 6],
           [2, 1, 7, 4, 5, 9, 6],
@@ -6,7 +8,7 @@ array1 = [[0, 1, 3, 4, 5, 6, 6],
           [15, 1, 3, 11, 5, 9, 5]]
 
 array2 = [[3, -5],
-          [1, -2],]
+          [1, -2], ]
 
 array3 = [[3, 3, 3, 3, 5, 6, 6],
           [2, 1, 7, 66, 5, 9, 6],
@@ -25,8 +27,14 @@ m4 = Matrix(array4)
 
 
 def test_add():
-    assert (m1 + m3).to_list() == [[3, 4, 6, 7, 10, 12, 12], [4, 2, 14, 70, 10, 18, 12], [0, 2, 6, 80, 10, 12, 12], [30, 2, 26, 442, 10, 18, 10]]
-    assert (m1 + m4).to_list() == [[3, 4, 6, 7, 10, 12, 12], [2, 1, 14, 4, 10, 18, 12], [121, 2, 6, 80, 48, 903, 57682], [30, 2, 26, 442, 10, 18, 10]]
+    """
+    Осуществляет проверку сложения двух матриц (работы функции add).
+    :return: "Add test complete".
+    """
+    assert (m1 + m3).to_list() == [[3, 4, 6, 7, 10, 12, 12], [4, 2, 14, 70, 10, 18, 12], [0, 2, 6, 80, 10, 12, 12],
+                                   [30, 2, 26, 442, 10, 18, 10]]
+    assert (m1 + m4).to_list() == [[3, 4, 6, 7, 10, 12, 12], [2, 1, 14, 4, 10, 18, 12], [121, 2, 6, 80, 48, 903, 57682],
+                                   [30, 2, 26, 442, 10, 18, 10]]
 
     try:
         m = m1 + m2
@@ -37,8 +45,14 @@ def test_add():
 
 
 def test_sub():
-    assert (m1 - m3).to_list() == [[-3, -2, 0, 1, 0, 0, 0], [0, 0, 0, -62, 0, 0, 0], [0, 0, 0, -60, 0, 0, 0], [0, 0, -20, -420, 0, 0, 0]]
-    assert (m1 - m4).to_list() == [[-3, -2, 0, 1, 0, 0, 0], [2, 1, 0, 4, 0, 0, 0], [-121, 0, 0, -60, -38, -891, -57670], [0, 0, -20, -420, 0, 0, 0]]
+    """
+    Осуществляет проверку вычитания двух матриц (работы функции sub).
+    :return: "Sub test complete".
+    """
+    assert (m1 - m3).to_list() == [[-3, -2, 0, 1, 0, 0, 0], [0, 0, 0, -62, 0, 0, 0], [0, 0, 0, -60, 0, 0, 0],
+                                   [0, 0, -20, -420, 0, 0, 0]]
+    assert (m1 - m4).to_list() == [[-3, -2, 0, 1, 0, 0, 0], [2, 1, 0, 4, 0, 0, 0], [-121, 0, 0, -60, -38, -891, -57670],
+                                   [0, 0, -20, -420, 0, 0, 0]]
 
     try:
         m = m1 - m2
@@ -49,6 +63,10 @@ def test_sub():
 
 
 def test_mul():
+    """
+    Осуществляет проверку умножения двух матриц или матрицы на число (работы функции mul).
+    :return: "Multiply test complete".
+    """
     assert (m1 * m1).to_list() == [[62, 8, 28, 78], [62, 14, 46, 126], [152, 14, 46, 144], [167, 30, 94, 215]]
     assert (m1 * m3).to_list() == [[62, 8, 108, 2000], [68, 18, 126, 2286], [152, 14, 246, 4586], [212, 60, 314, 5062]]
 
@@ -61,12 +79,16 @@ def test_mul():
 
 
 def test_slice():
+    """
+    Осуществляет проверку вывода строк и столбцов матрицы (работы функции getitem).
+    :return: "Slice test complete".
+    """
     assert m1[0:1:] == [[0, 1, 3, 4, 5, 6, 6]]
     assert m2[0:1:] == [[3, -5]]
 
     try:
         m = m1[0:1000:]
-        m = m1[1,43]
+        m = m1[1, 43]
         m = m1['a']
         m = m1[100]
     except TypeError:
@@ -76,14 +98,30 @@ def test_slice():
 
 
 def test_transposition():
-    assert m1.trans().to_list() == [[0, 2, 0, 15], [1, 1, 1, 1], [3, 7, 3, 3], [4, 4, 10, 11], [5, 5, 5, 5], [6, 9, 6, 9], [6, 6, 6, 5]]
-    assert m3.trans().to_list() == [[3, 2, 0, 15], [3, 1, 1, 1], [3, 7, 3, 23], [3, 66, 70, 431], [5, 5, 5, 5], [6, 9, 6, 9], [6, 6, 6, 5]]
+    """
+    Осуществляет проверку транспонирования матрицы (работы функции trans).
+    :return: "Transposition test complete".
+    """
+    assert m1.trans().to_list() == [[0, 2, 0, 15], [1, 1, 1, 1], [3, 7, 3, 3], [4, 4, 10, 11], [5, 5, 5, 5],
+                                    [6, 9, 6, 9], [6, 6, 6, 5]]
+    assert m3.trans().to_list() == [[3, 2, 0, 15], [3, 1, 1, 1], [3, 7, 3, 23], [3, 66, 70, 431], [5, 5, 5, 5],
+                                    [6, 9, 6, 9], [6, 6, 6, 5]]
+    try:
+        assert m1.trans().to_list() == [1, 2]
+    except AssertionError:
+        pass
 
     print("Transposition test complete")
 
+
 def test_delete():
-    assert m1.delete(1, 2).to_list() == [[2, 7, 4, 5, 9, 6], [0, 3, 10, 5, 6, 6], [15, 3, 11, 5, 9, 5]]
-    assert m3.delete(1, 3).to_list() == [[2, 1, 66, 5, 9, 6], [0, 1, 70, 5, 6, 6], [15, 1, 431, 5, 9, 5]]
+    """
+    Осуществляет проверку удаления строки или столбца матрицы (работы функции delete).
+    :return: "Delete test complete".
+    """
+    assert m1.delete("строка", 1).to_list() == [[2, 1, 7, 4, 5, 9, 6], [0, 1, 3, 10, 5, 6, 6], [15, 1, 3, 11, 5, 9, 5]]
+    assert m1.delete("столбец", 1).to_list() == [[1, 3, 4, 5, 6, 6], [1, 7, 4, 5, 9, 6], [1, 3, 10, 5, 6, 6],
+                                                 [1, 3, 11, 5, 9, 5]]
 
     try:
         m1.delete(10, 1)
@@ -94,19 +132,42 @@ def test_delete():
 
 
 def test_determinant():
-    assert m2.__invert__() == -1
+    """
+    Осуществляет проверку нахождения детерминанта матрицы (работы функции invert).
+    :return: "Determinant test complete".
+    """
+    assert ~m2 == -1
+
+    try:
+        assert ~m1
+    except ValueError:
+        pass
 
     print("Determinant test complete")
 
 
 def test_size():
+    """
+    Осуществляет проверку нахождения размера матрицы (работы функции size).
+    :return: "Size test complete".
+    """
     assert m1.size() == f"{len(m1.to_list())} строк, {len(m1.to_list()[0])} столбцов"
+
+    try:
+        m1.size() == 12
+    except AssertionError:
+        pass
 
     print("Size test complete")
 
 
 def test_permutation():
-    assert m1.permutation("строка", 1, 2).to_list() == [[2, 1, 7, 4, 5, 9, 6], [0, 1, 3, 4, 5, 6, 6], [0, 1, 3, 10, 5, 6, 6], [15, 1, 3, 11, 5, 9, 5]]
+    """
+    Осуществляет проверку перестановки строк или столбцов матрицы (работы функции permutation).
+    :return: "Permutation test complete".
+    """
+    assert m1.permutation("строка", 1, 2).to_list() == [[2, 1, 7, 4, 5, 9, 6], [0, 1, 3, 4, 5, 6, 6],
+                                                        [0, 1, 3, 10, 5, 6, 6], [15, 1, 3, 11, 5, 9, 5]]
 
     try:
         m = m1.permutation("строка", 1, 70)
@@ -117,30 +178,52 @@ def test_permutation():
 
 
 def test_reverse():  # **(-1)
-    assert (m2 ** (-1)).to_list() == [[ 2 -5][ 1 -3]]
+    """
+    Осуществляет проверку нахождения обратной матрицы (работы функции pow).
+    :return: "Reverse test complete".
+    """
+    assert (m2 ** -1).to_list() == [[2, -5], [1, -3]]
 
     try:
         m = m1 ** -1
-    except ValueError:
+    except numpy.linalg.LinAlgError:
         pass
 
     print("Reverse test complete")
 
 
 def test_rang():
+    """
+    Осуществляет проверку нахождения ранга матрицы (работы функции rang).
+    :return: "Rang test complete".
+    """
     assert m1.rang() == 4
     assert m2.rang() == 2
     assert m3.rang() == 4
     assert m4.rang() == 4
 
+    try:
+        assert m1.rang() == 1
+    except AssertionError:
+        pass
+
     print("Rank test complete")
 
 
 def test_to_list():
+    """
+    Осуществляет проверку вывода матрицы как массив (работы функции to_list).
+    :return: "Matrix to array test complete".
+    """
     assert m1.to_list() == array1
     assert m2.to_list() == array2
     assert m3.to_list() == array3
     assert m4.to_list() == array4
+
+    try:
+        assert m1.to_list() == [1, 2, 3]
+    except AssertionError:
+        pass
 
     print("Matrix to array test complete")
 
